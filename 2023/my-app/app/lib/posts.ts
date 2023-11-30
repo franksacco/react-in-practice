689984a9a2525d20353f4ec49aabaeea0264baed
Francesco Saccani <francesco.saccani@unipr.it>
2023-11-30 15:41:43 +0000

    Implement post functions

---
+++ 2023/my-app/app/lib/posts.ts
@@ -0,0 +1,15 @@
+import { Post } from '@/app/lib/types'
+import { promises as fs } from 'fs'
+
+export async function getPosts() {
+    const file = await fs.readFile(process.cwd() + '/app/lib/data.json', 'utf-8')
+    const data: Post[] = JSON.parse(file)
+    return data
+}
+
+export async function getPost(id: number) {
+    const file = await fs.readFile(process.cwd() + '/app/lib/data.json', 'utf-8')
+    const data: Post[] = JSON.parse(file)
+    const post = data.filter(p => p.id === id)
+    return post.length === 0 ? null : post[0]
+}
