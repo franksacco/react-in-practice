ac5da7b1448c983a004c1f4fad365a7583aa41f3
Francesco Saccani <francesco.saccani@unipr.it>
2023-12-01 14:11:18 +0000

    Add `PostDetails` component

---
+++ 2023/my-app/app/components/posts/PostDetails.tsx
@@ -0,0 +1,20 @@
+import { Post } from '@/app/lib/types'
+
+export default function PostDetails({ post }: { post: Post }) {
+    return (
+        <article>
+            <h1 className="font-light text-4xl my-4">
+                {post.title}
+            </h1>
+            <section className="font-bold text-xl text-gray-600">
+                {post.author}
+            </section>
+            <section className="text-right text-gray-500 text-sm italic my-4">
+                {post.created}
+            </section>
+            <section>
+                {post.content}
+            </section>
+        </article>
+    )
+}
