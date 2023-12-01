8ca6acd6b75b3552b31885fa3d35dff91ed6be79
Francesco Saccani <francesco.saccani@unipr.it>
2023-12-01 14:17:52 +0000

    Implement posts page

--- 2023/my-app/app/posts/page.tsx
+++ 2023/my-app/app/posts/page.tsx
@@ -1,5 +1,20 @@
-export default function Posts() {
+import PostPreview from "../components/posts/PostPreview"
+import { getPosts } from "../lib/posts"
+
+export default async function Posts() {
+    const posts = await getPosts()
     return (
-        <h1>List of posts</h1>
+        <>
+            <h1 className="font-light text-4xl mt-4 mb-6">
+                Blog Posts
+            </h1>
+            <div className="border-t">
+                {posts.map((p, i) => (
+                    <div className="border-b py-2" key={i}>
+                        <PostPreview post={p} />
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
