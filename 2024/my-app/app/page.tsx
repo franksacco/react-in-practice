48bb0ff872ba692f86401901f6a094a8aa07c0f8
Francesco Saccani <francesco.saccani@unipr.it>
2024-12-04 15:07:58 +0000

    Cleanup

--- 2024/my-app/app/page.tsx
+++ 2024/my-app/app/page.tsx
@@ -1,34 +1,5 @@
-"use client";
-
-import { useState } from "react";
-
-const TrackCursor = () => {
-  const [cursor, setCursor] = useState({ x: 0, y: 0 });
-
-  return (
-    <div
-      className="h-screen w-screen relative"
-      onMouseMove={e => setCursor({ x: e.clientX, y: e.clientY })}
-    >
-      <div className="p-4">
-        Cursor: {cursor.x}, {cursor.y}
-      </div>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'red',
-        borderRadius: '50%',
-        transform: `translate(${cursor.x}px, ${cursor.y}px)`,
-        left: -10,
-        top: -10,
-        width: 20,
-        height: 20,
-      }} />
-    </div>
-  );
-}
-
 export default function Home() {
   return (
-    <TrackCursor />
+    <section></section>
   );
 }
