2e7fc7d4eccabb3f7206d738fa5cc57b0cd55be7
Francesco Saccani <francesco.saccani@unipr.it>
2024-12-04 16:25:29 +0000

    Root layout

--- 2024/my-app/app/layout.tsx
+++ 2024/my-app/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
+import Image from "next/image";
+import { Nunito } from "next/font/google";
 import "./globals.css";
+
+const inter = Nunito({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,8 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className={inter.className + " bg-slate-100"}>
+        <div className="max-w-2xl mx-auto my-8">
+          <header className="flex flex-col items-center">
+            <Image
+              alt="logo"
+              className="rounded-full"
+              height={150}
+              width={150}
+              src="/logo.png"
+            />
+            <span className="my-4 text-4xl">
+              My Blog
+            </span>
+          </header>
+          <main className="bg-white p-8">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
