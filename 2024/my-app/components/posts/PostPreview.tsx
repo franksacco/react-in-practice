e1cd05367b5e6c4431ba51ebed6ec7ac6c668bc9
Francesco Saccani <francesco.saccani@unipr.it>
2024-12-04 16:53:32 +0000

    Add post components

---
+++ 2024/my-app/components/posts/PostPreview.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+import type { Post } from "@/lib/posts";
+
+export default function PostPreview({ post }: { post: Post }) {
+  return (
+    <Link href={'/posts/' + post.id}>
+      <div className="flex flex-col p-2">
+        <span className="font-bold text-xl">
+          {post.title}
+        </span>
+        <span className="text-gray-600">
+          {post.created} | {post.author}
+        </span>
+      </div>
+    </Link>
+  )
+}
