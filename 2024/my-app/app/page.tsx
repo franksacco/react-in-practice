97085e660a60d13ec32da7571c30953965333276
Francesco Saccani <francesco.saccani@unipr.it>
2024-12-04 16:26:00 +0000

    Static routes

--- 2024/my-app/app/page.tsx
+++ 2024/my-app/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <section></section>
+    <section>Home</section>
   );
 }
---
+++ 2024/my-app/app/posts/page.tsx
@@ -0,0 +1,5 @@
+export default function Posts() {
+  return (
+    <section>Posts</section>
+  );
+}
